d856a5198f5962b1d0d76bbb19c4801138bbcc15
A <alhamc@gmail.com>
2021-06-19 16:41:44 -0400

    api documentation commented

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,13 +6,12 @@ export default function Dictionary (){
 let [keyword, setkeyword]= useState ("");
 
 function handleResponse(response) {
-    console.log (response);
+    console.log (response.data[0]);
 }
 
 function search(event){
     event.preventDefault();
-    alert (`searching for ${keyword} definition`);
-
+//api documentation:https://dictionaryapi.dev
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 }
