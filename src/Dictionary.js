901c37ecc99b83b63d5c9fb8197e7786e32748f1
A <alhamc@gmail.com>
2021-06-26 16:59:38 -0400

    added photos using pexel Api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,30 +2,40 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary (props){
 let [keyword, setkeyword]= useState (props.defaultKeyword);
 let [results, setResults]= useState (null);
 let [loaded, setLoaded]=useState (false);
+let [photos, setPhotos] = useState(null);
 
-function handleResponse(response) {
+function handleDictionaryResponse(response) {
     setResults (response.data[0]);
     console.log (response.data[0]);
 }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
 function search (){
   //api documentation:https://dictionaryapi.dev
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);  
+    axios.get(apiUrl).then(handleDictionaryResponse);  
 
+    let pexelsApiKey=
+    `563492ad6f9170000100000167379337e9dc48ed89d26eb79b1c7dec`;
+    let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 }
+
 
 function handleSubmit(event){
     event.preventDefault();
     search();
-
 }
-
 
 function handleKeywordChange (event){
      setkeyword(event.target.value);
@@ -34,7 +44,6 @@ function handleKeywordChange (event){
 function load(){
     setLoaded (true);
     search();
-
 }
 
 if (loaded){
@@ -47,20 +56,19 @@ return (
                 onChange = {handleKeywordChange} 
                 defaultValue ={props.defaultKeyword}/>
             </form>
-            <div className=
-            "hint">
+            <div className="hint">
                suggested words: sunset, wine, yoga...
             </div>
             </section>
             <section>
                 <Results results = {results}/>
-            </section>
-            
+                <Photos photos = {photos}/>
+            </section>   
         </div>
         );
 } else {
-
-} load();
+load();
 return "loading...";
+}
     
 }
